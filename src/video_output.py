11755e9f3cb578597e2e8f94adcb6108f0056bbc
LucasAurouet <lucas.aurouet@hotmail.fr>
2025-12-17 17:27:11 +0100

    first test with webcam real time input

--- src/video_output.py
+++ src/video_output.py
@@ -11,30 +11,36 @@ def inference(model, frame):
     return results[0].boxes
 
 def draw_boxes(frame, boxes):
-        for box, conf, label in zip(boxes.xyxy, boxes.conf, boxes.cls):
-            x1, y1, x2, y2 = map(int, box)
-            # Draw the box
-            color = (0,255,0)
-            cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)
-            cv2.putText(frame, f"{label}:{conf:.2f}", (x1, y1-5),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+    for box, conf, label in zip(boxes.xyxy, boxes.conf, boxes.cls):
+        x1, y1, x2, y2 = map(int, box)
+        # Draw the box
+        color = (0,255,0)
+        cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)
+        cv2.putText(frame, f"{label}:{conf:.2f}", (x1, y1-5),
+                   cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
-        # Show the image 
-        cv2.imshow('video', frame)
+    # Show the image 
+    cv2.imshow('video', frame)
 
 def read_stream(model=None):
 
     picam2 = Picamera2()
 
     # Configuration "still" ou "preview"
-    preview_config = picam2.create_preview_configuration()
+    preview_config = picam2.create_video_configuration(
+        main={"size": (480, 480),
+              "format": "RGB888"
+              }
+        )
     picam2.configure(preview_config)
 
     picam2.start()
     time.sleep(2)
     
     while True:
-        frame = cv2.resize(picam2.capture_array(), 640, 640)
+        #frame = cv2.resize(picam2.capture_array(), 640, 640)
+        frame = picam2.capture_array()
+        print(frame.shape)
         
         if model != None:
             boxes = inference(model, frame)
@@ -48,10 +54,10 @@ def read_stream(model=None):
     picam2.stop()
     cv2.destroyAllWindows()
     
-model = YOLO('C:\\Users\\Lucas\\Desktop\\yolov8-realtime-detection\\weights\\finetuned_weights.pt')
+model = YOLO('/home/lucas/Desktop/yolov8-raspberry-pi/weights/finetuned_weights.pt')
 
-img_test = cv2.imread("/home/lucas/Desktop/yolov8-raspberry-pi/test_image.jpg")
-results = model(img_test)
-draw_boxes(img_test, results[0].boxes)
+#img_test = cv2.imread(r"/home/lucas/Desktop/yolov8-raspberry-pi/test_image.jpg")
+#results = model(img_test)
+#draw_boxes(img_test, results[0].boxes)
 
-# read_stream(model=model)
+read_stream(model=model)
